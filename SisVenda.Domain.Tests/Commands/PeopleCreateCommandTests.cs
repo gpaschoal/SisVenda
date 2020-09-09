5f9cc5fb79ac4129d4305d756501031d4b434c44
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-09-08 22:23:30 -0300

    Test Validate should fail when the name does not require min length

---
+++ SisVenda.Domain.Tests/Commands/PeopleCreateCommandTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class PeopleCreateCommandTests
+    {
+        public PeopleCreateCommandTests()
+        {
+        }
+        private PeopleCreateCommand MakeValidPeopleCreateCommand() => new PeopleCreateCommand(
+                true,
+                true,
+                "name",
+                "contact",
+                "cpf",
+                "cnpj",
+                "street",
+                "number",
+                "neighborhood",
+                "valid_city",
+                "UF",
+                "zipCode",
+                "adressEmail@mail.com",
+                "phoneNumber");
+
+        [TestMethod]
+        public void Validation_should_fail_when_the_name_does_not_require_min_length()
+        {
+            var invalidCommand = MakeValidPeopleCreateCommand();
+            invalidCommand.Name = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
