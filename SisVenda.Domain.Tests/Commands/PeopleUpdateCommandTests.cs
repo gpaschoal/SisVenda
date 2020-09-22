e964e1faead1be8b98daa67ff0b0a7299abfe905
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-09-21 21:16:27 -0300

    Should fail when PeopleUpdateCommand Id is empty

---
+++ SisVenda.Domain.Tests/Commands/PeopleUpdateCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class PeopleUpdateCommandTests
+    {
+        public PeopleUpdateCommandTests() { }
+        private PeopleUpdateCommand MakeValidPeopleUpdateCommand() => new PeopleUpdateCommand(
+                 "valid_id", 
+                 true, 
+                 true, 
+                 "name", 
+                 "contact", 
+                 "cpf", 
+                 "cnpj", 
+                 "street",
+                 "number", 
+                 "neighborhood", 
+                 "city", 
+                 "UF", 
+                 "zipCode", 
+                 "adressEmail@mail.com", 
+                 "phoneNumber"
+            );
+        [TestMethod]
+        public void Should_fail_when_Id_is_empty()
+        {
+            var invalidCommand = MakeValidPeopleUpdateCommand();
+            invalidCommand.Id = "";
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Id", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
