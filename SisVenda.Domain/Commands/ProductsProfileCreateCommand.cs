8edd0ce7f78c9847d386f89f2b1bc2e7fd3a28f4
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 08:52:35 -0300

    Refactor ProductsProfileCreateCommand validation

--- SisVenda.Domain/Commands/ProductsProfileCreateCommand.cs
+++ SisVenda.Domain/Commands/ProductsProfileCreateCommand.cs
@@ -6,7 +6,6 @@ namespace SisVenda.Domain.Commands
 {
     public class ProductsProfileCreateCommand : Notifiable, ICommand
     {
-
         public ProductsProfileCreateCommand() { }
         public string UnitMeasurementId { get; set; }
         public string ProductsId { get; set; }
@@ -18,12 +17,7 @@ namespace SisVenda.Domain.Commands
                     .Requires()
                     .IsNotNullOrEmpty(UnitMeasurementId, "UnitMeasurementId", "O código da unidade de medida é inválida!")
                     .IsNotNullOrEmpty(ProductsId, "ProductsId", "O código do produto é inválido!")
-                    //.HasMinLen(UnitMeasurementId, 32, "UnitMeasurementId", "A Unidade de medida é inválida!")
-                    //.HasMaxLen(UnitMeasurementId, 32, "UnitMeasurementId", "A Unidade de medida é inválida!")
-                    //.HasMinLen(ProductsId, 32, "ProductsId", "O Produto é inválido!")
-                    //.HasMaxLen(ProductsId, 32, "ProductsId", "O Produto é inválido!")
-                    .HasMinLen(BarCode, 003, "BarCode", "O código de barras precisa ter no mínimo 3 digitos!")
-                    .HasMaxLen(BarCode, 100, "BarCode", "O código de barras precisa ter no máximo 100 digitos!")
+                    .IsBetween(BarCode?.Trim().Length ?? 0, 003, 100, "BarCode", "O código de barras precisa ter no entre 3 e 100 digitos!")
             );
         }
     }
