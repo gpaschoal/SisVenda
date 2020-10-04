f1ed63eb55bd4f554bc49830448d2974814d40ec
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-03 23:57:34 -0300

    Test ProductsCreateCommand fails when description exceeds max length

--- SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
@@ -62,5 +62,15 @@ namespace SisVenda.Domain.Tests.Commands
 
             Assert.AreEqual("Description", invalidCommand.Notifications.First().Property);
         }
+
+        [TestMethod]
+        public void Should_fail_when_the_description_exceeds_max_length()
+        {
+            var invalidCommand = MakeValidProductsCreateCommand();
+            invalidCommand.Description = "".PadLeft(151, '0');
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Description", invalidCommand.Notifications.First().Property);
+        }
     }
 }
