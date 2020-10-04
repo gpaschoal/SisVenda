1152a03812485d3feb0dd77192454d254ecd9fda
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 14:06:40 -0300

    Test UnitMeasurementCreateCommand validations fail when name is null

---
+++ SisVenda.Domain.Tests/Commands/UnitMeasurementCreateCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class UnitMeasurementCreateCommandTests
+    {
+        public UnitMeasurementCreateCommandTests() { }
+        private UnitMeasurementCreateCommand MakeUnitMeasurementCreateCommand() => new UnitMeasurementCreateCommand("", 1);
+
+        [TestMethod]
+        public void Should_fail_when_Name_is_null()
+        {
+            var invalidCommand = MakeUnitMeasurementCreateCommand();
+            invalidCommand.Name = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.Single().Property);
+        }
+    }
+}
