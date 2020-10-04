994236560d662db7151b4b51613f9b668ee476cf
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 00:21:31 -0300

    Test ProductsUpdateCommand fails when Name is null

---
+++ SisVenda.Domain.Tests/Commands/ProductsUpdateCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class ProductsUpdateCommandTests
+    {
+        public ProductsUpdateCommandTests() { }
+
+        private ProductsUpdateCommand ProductsUpdateCommandCommand() => new ProductsUpdateCommand("valid_id", "Name", "Description");
+
+        [TestMethod]
+        public void Should_fail_when_the_name_is_null()
+        {
+            var invalidCommand = ProductsUpdateCommandCommand();
+            invalidCommand.Name = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
