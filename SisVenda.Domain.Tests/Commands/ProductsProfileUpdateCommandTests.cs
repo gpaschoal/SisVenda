cda429a686cb5fd17560a2ad2cdb23fdcf07d9e0
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 10:37:37 -0300

    Test ProductsProfileUpdateCommand validation fail when id is empty

---
+++ SisVenda.Domain.Tests/Commands/ProductsProfileUpdateCommandTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class ProductsProfileUpdateCommandTests
+    {
+        public ProductsProfileUpdateCommandTests() { }
+
+        private ProductsProfileUpdateCommand MakeProductsProfileUpdateCommand() => new ProductsProfileUpdateCommand("valid_id", "validunitmeasurementid", "validproductid", "validbarcode");
+
+        [TestMethod]
+        public void Should_fail_when_id_is_null()
+        {
+            var invalidCommand = MakeProductsProfileUpdateCommand();
+            invalidCommand.Id = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Id", invalidCommand.Notifications.First().Property);
+        }
+
+        [TestMethod]
+        public void Should_fail_when_id_is_empty()
+        {
+            var invalidCommand = MakeProductsProfileUpdateCommand();
+            invalidCommand.Id = "";
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Id", invalidCommand.Notifications.Single().Property);
+        }
+    }
+}
