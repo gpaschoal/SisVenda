15c4309d3166082ce55870efeae64901ad272420
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 00:37:12 -0300

    Test ProductsUpdateCommand fails when name exceeds max len

--- SisVenda.Domain.Tests/Commands/ProductsUpdateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsUpdateCommandTests.cs
@@ -32,6 +32,16 @@ namespace SisVenda.Domain.Tests.Commands
         }
 
         [TestMethod]
+        public void Should_fail_when_the_name_exceeds_max_length()
+        {
+            var invalidCommand = MakeProductsUpdateCommandCommand();
+            invalidCommand.Name = "".PadLeft(151, '0');
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
+
+        [TestMethod]
         public void Should_fail_when_id_is_empty()
         {
             var invalidCommand = MakeProductsUpdateCommandCommand();
