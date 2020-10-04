91edfa76efacba3516410d4bafbc20838973d812
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 00:22:10 -0300

    Test ProductsUpdateCommand fails when name is empty

--- SisVenda.Domain.Tests/Commands/ProductsUpdateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsUpdateCommandTests.cs
@@ -20,5 +20,15 @@ namespace SisVenda.Domain.Tests.Commands
 
             Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
         }
+
+        [TestMethod]
+        public void Should_fail_when_the_name_is_empty()
+        {
+            var invalidCommand = ProductsUpdateCommandCommand();
+            invalidCommand.Name = "";
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
     }
 }
