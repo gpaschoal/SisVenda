e2331091aa1cbb3ced402452e42259eb53c3b221
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 14:18:28 -0300

    Refactor UnitMeasurementCreateCommand validation

--- SisVenda.Domain/Commands/UnitMeasurementCreateCommand.cs
+++ SisVenda.Domain/Commands/UnitMeasurementCreateCommand.cs
@@ -14,14 +14,14 @@ namespace SisVenda.Domain.Commands
         }
 
         public string Name { get; set; }
-        public double QuantityLosses { get; set; }
+        public double? QuantityLosses { get; set; }
         public void Validate()
         {
             AddNotifications(
                new Contract()
                    .Requires()
-                   .IsBetween(Name?.Trim().Length ?? 0, 3, 150, "Name", "O Nome precisa ter pelo menos 3 dígitos")
-                   .IsGreaterThan(QuantityLosses, 0.1, "QuantityLosses", "A quantidade não pode ser menor que 0")
+                   .IsBetween(Name?.Trim().Length ?? 0, 3, 150, "Name", "O Nome precisa ter pelo entre 3 e 150 dígitos")
+                   .IsGreaterThan(QuantityLosses ?? 0, 0.1d, "QuantityLosses", "A quantidade não pode ser menor que 0")
            );
         }
     }
