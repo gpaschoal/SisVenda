4cea500cc8e6929a2a35517199c70144506b06db
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 00:05:55 -0300

    Test ProductsDeleteCommand fails when id is empty

---
+++ SisVenda.Domain.Tests/Commands/ProductsDeleteCommandTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class ProductsDeleteCommandTests
+    {
+        public ProductsDeleteCommandTests()
+        {
+
+        }
+        private ProductsDeleteCommand MakeValidProductsDeleteCommand() => new ProductsDeleteCommand("valid_id");
+
+        [TestMethod]
+        public void Should_fail_when_id_is_empty()
+        {
+            var invalidCommand = MakeValidProductsDeleteCommand();
+            invalidCommand.Id = "";
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Id", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
