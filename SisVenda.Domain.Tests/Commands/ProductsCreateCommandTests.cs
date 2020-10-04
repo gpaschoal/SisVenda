41261eae961c631d227341a710962ad8b3cef91c
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-03 23:53:04 -0300

    Test ProductsCreateCommand fails when Name is null

--- SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SisVenda.Domain.Commands;
+using System.Linq;
 
 namespace SisVenda.Domain.Tests.Commands
 {
@@ -11,5 +12,15 @@ namespace SisVenda.Domain.Tests.Commands
 
         }
         private ProductsCreateCommand MakeValidProductsCreateCommand() => new ProductsCreateCommand("Name", "Description");
+
+        [TestMethod]
+        public void Should_fail_when_the_name_is_null()
+        {
+            var invalidCommand = MakeValidProductsCreateCommand();
+            invalidCommand.Name = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
     }
 }
