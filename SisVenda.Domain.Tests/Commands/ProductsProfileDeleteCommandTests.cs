3da4134512c49c7bb898540d2e7b17d0b7bc02d5
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 09:37:30 -0300

    Test ProductsProfileDeleteCommand validation fails when id is null

---
+++ SisVenda.Domain.Tests/Commands/ProductsProfileDeleteCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class ProductsProfileDeleteCommandTests
+    {
+        public ProductsProfileDeleteCommandTests() { }
+        private ProductsProfileDeleteCommand MakeProductsProfileDeleteCommand() => new ProductsProfileDeleteCommand("valid_id");
+
+        [TestMethod]
+        public void Should_fail_when_id_is_null()
+        {
+            var invalidCommand = MakeProductsProfileDeleteCommand();
+            invalidCommand.Id = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Id", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
