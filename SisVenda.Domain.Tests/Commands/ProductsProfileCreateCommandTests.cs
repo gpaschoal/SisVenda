cc513a1fe4e06f85a3d5d0e50498cef06f5e55e3
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 09:07:56 -0300

    Test ProductsProfileCreateCommand validation fails when ProductsId is null

--- SisVenda.Domain.Tests/Commands/ProductsProfileCreateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsProfileCreateCommandTests.cs
@@ -29,5 +29,15 @@ namespace SisVenda.Domain.Tests.Commands
 
             Assert.AreEqual("UnitMeasurementId", invalidCommand.Notifications.First().Property);
         }
+
+        [TestMethod]
+        public void Should_fail_when_ProductsId_is_null()
+        {
+            var invalidCommand = MakeProductsProfileCreateCommand();
+            invalidCommand.ProductsId = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("ProductsId", invalidCommand.Notifications.First().Property);
+        }
     }
 }
