77c71f8c3a5749f6be7efa7466c1af5099fc2656
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 09:06:23 -0300

    Test ProductsProfileCreateCommand fails when UnitMeasurementId is null

---
+++ SisVenda.Domain.Tests/Commands/ProductsProfileCreateCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Commands;
+using System.Linq;
+
+namespace SisVenda.Domain.Tests.Commands
+{
+    [TestClass]
+    public class ProductsProfileCreateCommandTests
+    {
+        public ProductsProfileCreateCommandTests() { }
+        private ProductsProfileCreateCommand MakeProductsProfileCreateCommand() => new ProductsProfileCreateCommand("validunitmeasurementid", "validproductid", "validbarcode");
+
+        [TestMethod]
+        public void Should_fail_when_UnitMeasurementId_is_null()
+        {
+            var invalidCommand = MakeProductsProfileCreateCommand();
+            invalidCommand.UnitMeasurementId = null;
+            invalidCommand.Validate();
+
+            Assert.AreEqual("UnitMeasurementId", invalidCommand.Notifications.First().Property);
+        }
+    }
+}
