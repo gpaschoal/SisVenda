bed2cc5645001ceb9780dfac66ec0d181b2b01ad
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-03 23:55:06 -0300

    Test ProductsCreateCommand Fails when Name exceeds max length

--- SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
+++ SisVenda.Domain.Tests/Commands/ProductsCreateCommandTests.cs
@@ -22,11 +22,22 @@ namespace SisVenda.Domain.Tests.Commands
 
             Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
         }
+
         [TestMethod]
         public void Should_fail_when_the_name_is_empty()
         {
             var invalidCommand = MakeValidProductsCreateCommand();
             invalidCommand.Name = "";
+            invalidCommand.Validate();
+
+            Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
+        }
+
+        [TestMethod]
+        public void Should_fail_when_the_name_exceeds_max_length()
+        {
+            var invalidCommand = MakeValidProductsCreateCommand();
+            invalidCommand.Name = "".PadLeft(151, '0');
             invalidCommand.Validate();
 
             Assert.AreEqual("Name", invalidCommand.Notifications.First().Property);
