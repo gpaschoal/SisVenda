3e8a84d9f0946744d0af4dbf30c91498545d8a96
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 22:54:37 -0300

    Refactor Test class name

---
+++ SisVenda.Domain.Tests/Entities/BankTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Entities;
+
+namespace SisVenda.Domain.Tests.Entities
+{
+    [TestClass]
+    public class BankTests
+    {
+        public BankTests() { }
+
+        [TestMethod]
+        public void Should_be_equal_parameters_and_properties()
+        {
+            var name = "Name";
+            var code = "Code";
+
+            var bank = new Bank(name, code);
+
+            Assert.AreEqual(
+                (name, code),
+                (bank.Name, bank.Code)
+            );
+        }
+    }
+}
