f2fa2a159816f007e594c97b038049038d5af24c
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-10-04 22:56:21 -0300

    Test after Entity is created id should has a valid value

---
+++ SisVenda.Domain.Tests/Entities/Base/EntityTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SisVenda.Domain.Base.Entities;
+
+namespace SisVenda.Domain.Tests.Entities.Base
+{
+    [TestClass]
+    public class EntityTests
+    {
+        private class EntityStub : Entity { }
+
+        public EntityTests() { }
+
+        [TestMethod]
+        [DataRow("")]
+        [DataRow(null)]
+        public void After_create_id_should_has_value(string value)
+        {
+            var entityStub = new EntityStub();
+
+            Assert.AreNotEqual(value, entityStub.Id);
+        }
+    }
+}
